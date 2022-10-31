3be4f464216b2ba7dcb1da7bcb2db3fbef584ddd
Nathan Malone <nathanamalone@gmail.com>
2022-10-31 16:22:18 -0500

    through ch 5

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -5,8 +5,8 @@ from levelupapi.models.gamer import Gamer
 
 class Event(models.Model):
     game = models.ForeignKey('Game', on_delete=models.CASCADE)
-    organizer = models.ForeignKey('Gamer', on_delete=models.CASCADE)
+    organizer = models.ForeignKey('Gamer', on_delete=models.CASCADE, related_name='events')
     description = models.CharField(max_length=155)
     date = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
     time = models.TimeField(auto_now=False, auto_now_add=False)
-    gamers = models.ManyToManyField(Gamer)
+    gamers = models.ManyToManyField(Gamer, through='EventGamer')
