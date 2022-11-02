9e908367b55b2e85b1e5773ab1f8a48c49f6856a
Nathan Malone <nathanamalone@gmail.com>
2022-11-02 16:12:42 -0500

    need to add rest of forms

--- levelupapi/views/events.py
+++ levelupapi/views/events.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Event
+from levelupapi.models import Event, Game, Gamer
 
 class EventView(ViewSet):
     """Level up game types view"""
@@ -37,10 +37,48 @@ class EventView(ViewSet):
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
 
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        game = Game.objects.get(pk=request.data["game"])
+        organizer = Gamer.objects.get(pk=request.data["organizer"])
+
+        game = Event.objects.create(
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            game=game,
+            organizer=organizer
+        )
+        serializer = EventSerializer(game)
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        event = Event.objects.get(pk=pk)
+        event.description = request.data["description"]
+        event.date = request.data["date"]
+        event.time = request.data["time"]
+
+        game = Game.objects.get(pk=request.data["game"])
+        event.game = game
+        event.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
 class EventSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
     class Meta:
         model = Event
-        fields = ('id', 'game', 'organizer', 'description', 'date', 'time', 'gamers',)
-        depth = 1
+        fields = ('id', 'game', 'organizer', 'description', 'date', 'time', 'gamers', )
+        
